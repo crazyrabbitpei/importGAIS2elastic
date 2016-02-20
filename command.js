c127af79034bc814e9f76607edc7218878dba728
crazyrabbitpei <crazyrabbitpei@gmail.com>
2016-02-21 03:19:12 +0800

    for normal boards

---
+++ command.js
@@ -0,0 +1,49 @@
+var elasticsearch = require('elasticsearch');
+var fs = require('graceful-fs');
+var setting = JSON.parse(fs.readFileSync("config/setting"));
+
+var CronJob = require('cron').CronJob;
+var dateFormat = require('dateformat');
+
+var LineByLineReader = require('line-by-line');
+var S = require('string');
+const exec = require('child_process').exec;
+
+
+var ip = setting['db_ip'];
+var port = setting['db_port'];
+var dbname = setting['ptt_dbname'];
+var table = setting['ptt_table'];
+var column = setting['ptt_column'];
+var dataDir = setting['dataDir'];
+
+var import_record_nums=0;
+
+var client;
+var tag;
+
+connect2DB(ip,port,function(stat){
+    var table = process.argv[2];
+    var scolumn = process.argv[3];
+    var pattern = process.argv[4];
+    var fields = "";
+    search(dbname,table,scolumn,pattern,fields);
+});
+
+
+function connect2DB(dbip,dbport,fin){
+    client = new elasticsearch.Client({
+        host:dbip+':'+dbport,
+    });
+    fin("connect ok");
+}
+
+function search(dname,tname,scolumn,pattern,fields){
+    client.search({
+        index:dname,
+        type:tname,
+        q:scolumn+":"+pattern
+    },function(err,response){
+        console.log(response.hits.total);
+    });
+}
---
+++ run.js
@@ -0,0 +1,351 @@
+var elasticsearch = require('elasticsearch');
+var fs = require('graceful-fs');
+var setting = JSON.parse(fs.readFileSync("config/setting"));
+
+var CronJob = require('cron').CronJob;
+var dateFormat = require('dateformat');
+
+var LineByLineReader = require('line-by-line');
+var S = require('string');
+const exec = require('child_process').exec;
+var sleep = require('sleep'); 
+
+var ip = setting['db_ip'];
+var port = setting['db_port'];
+var dbname = setting['ptt_dbname'];
+var table = setting['ptt_table'];
+var column = setting['ptt_column'];
+var dataDir = setting['dataDir'];
+
+var import_record_nums=0;
+
+/*not yet*/
+var job = new CronJob({
+    cronTime:"50 59 23 * * *",
+    onTick:function(){
+        var date = dateFormat(new Date(), "yyyymmdd");
+        readlist(dataDir,date,function(fname,total_column){
+            for(i=0;i<fname.length-1;i++){
+                //console.log("["+i+"] "+fname[i]);
+                gais2json(total_column,fname[i],function(result){
+                    //console.log("result:"+result); 
+                });
+            } 
+        });
+    },
+    start:false,
+    timeZone:'Asia/Taipei'
+});
+
+var client;
+var tag;
+var import_again=0;
+
+connect2DB(ip,port,function(stat){
+    var date = dateFormat(new Date(), "yyyymmdd");
+    readlist(dataDir,table,function(fname,total_column){
+        var nums = fname.length-1;
+        var i=0;
+        var count_importfile=0;
+        /*
+        gais2json(total_column,fname[i],function(result){
+        });
+        fs.appendFile("logs/import_"+date+".list",fname[i]+"\n",function(err){
+            if(err){
+                //console.log("write log false:"+error);
+            }
+        });
+        */
+        var promise1 = new Promise(function(resolve,reject){
+            gais2json(total_column,fname[i],function(result){
+                resolve(result);   
+            });
+
+        });
+        promise1.then(function(value){
+            //console.log("["+value+"] done");
+            fs.appendFile("logs/import_"+date+".list",value+"\n",function(err){
+                if(err){
+                    //console.log("write log false:"+error);
+                }
+            });
+            if(count_importfile==nums){
+                console.log("All list imported.");
+            }
+        }).catch(function(error){
+            fs.appendFile("logs/err_"+date+".log",error+"\n",function(err){
+                if(err){
+                    //console.log("write log false:"+error);
+                }
+            });
+            clearInterval(tag);
+        });
+
+        i++;
+        count_importfile++;
+        if(count_importfile==nums){
+            console.log("All list imported.");
+            return;
+        }
+
+        tag = setInterval(function(){
+            var promise = new Promise(function(resolve,reject){
+                gais2json(total_column,fname[i],function(result){
+                    resolve(result);   
+                });
+            
+            });
+            promise.then(function(value){
+                //console.log("["+value+"] done");
+                fs.appendFile("logs/import_"+date+".list",value+"\n",function(err){
+                    if(err){
+                        //console.log("write log false:"+error);
+                    }
+                });
+
+                count_importfile++;
+                if(count_importfile==nums){
+                    console.log("All list imported.");
+                }
+            }).catch(function(error){
+                fs.appendFile("logs/err_"+date+".log",error+"\n",function(err){
+                    if(err){
+                        //console.log("write log false:"+error);
+                    }
+                });
+                clearInterval(tag);
+            });
+
+            i++;
+            if(i==nums){
+                //console.log("Stop interval and watting....");
+                clearInterval(tag);
+            }
+        },60*1000);
+        /*
+        for(i=0;i<fname.length-1;i++){
+            //console.log("["+i+"] "+fname[i]);
+            gais2json(total_column,fname[i],function(result){
+                //console.log("result:"+result); 
+            });
+        } 
+        */
+    });
+    //job.start();
+});
+
+
+function connect2DB(dbip,dbport,fin){
+    client = new elasticsearch.Client({
+        host:dbip+':'+dbport,
+    });
+    fin("connect ok");
+}
+
+function readlist(dir,filename,fin){
+//get all board, read in array, recording file name (use exec find . -name 2016* | grep -c ""   doesn't need _stop file)
+    //console.log(`find `+dir+` -name `+filename);
+    const child = exec(`find `+dir+` -name `+filename+" -not -name *_stop",(error,stdout,stderr) => {
+        if(error!=null){
+            console.log(`exec error: ${error}`);
+        }
+        else{
+            //console.log(`stdout: ${stdout}`);
+            var fname = stdout.split("\n");
+            var total_column = [];
+            var i;
+            var column_name = Object.keys(column);
+            column_name.forEach(function(cname){
+                var items = Object.keys(column[cname]);
+                items.forEach(function(item) {
+                        var value = column[cname][item];
+                        //console.log(cname+': '+item+' = '+value);
+                        total_column.push(item);
+
+                });
+            });
+            /*
+            for(i=0;i<total_column.length;i++){
+                console.log(total_column[i]+":"+total_column[i].length);
+            }
+            */
+           fin(fname,total_column);
+
+        }
+    });
+
+}
+
+function gais2json(cname,dir,fin){
+    //read file 2016...
+    readGaisdata(cname,dir,function(stat){
+        fin(stat);
+    });   
+}
+
+function readGaisdata(cname,filename,fin){
+    var i;
+    var body_flag=0;
+    var content = [];
+    var body="";
+
+    var options = {
+        skipEmptyLines:true
+    }
+    var lr = new LineByLineReader(filename,options);
+    lr.on('error', function (err) {
+        console.log("error:"+err);
+    });
+    lr.on('line', function (line) {
+        //cut and get column info
+        for(i=0;i<cname.length;i++){
+            if(line=="@"){
+                body_flag=0;
+                //conbert to json
+                if(body!=""){
+                    var record = JSON.stringify({
+                        title:content[0],
+                        source:content[1],
+                        url:content[2],
+                        time:content[3],
+                        body:body
+                    });
+                    body="";
+                    var tname = S(filename).right(8).s;
+                    tname = S(tname).left(6).s;//use YYYYMM ex:201602 for table's name
+
+                    import_record_nums++;
+                    setTimeout(function(){
+                        import2db(dbname,tname,record);
+                    },import_record_nums*1000);
+
+                    if(import_record_nums>600){
+                        import_record_nums=1;
+                        lr.pause();
+                        setTimeout(function(){
+                            lr.resume();
+                        },10*1000);
+
+                    }
+
+                    content = [];
+                    //console.log(record);
+                }
+                break;
+            }
+            else if(S(line).left(cname[i].length).s=="@body"&&cname[i]=="@body"){
+                body_flag=1;
+                //console.log(cname[i]+":");
+                break;
+            }
+            else if(body_flag==1){
+                //console.log(line);
+                body += line;
+
+                break;
+            }
+            else if(S(line).left(cname[i].length).s==cname[i]){
+                //console.log(cname[i]+":"+S(line).right(line.length-cname[i].length-1).s);
+                content.push(S(line).right(line.length-cname[i].length-1).s);
+                break;
+            }
+            
+        }
+
+    });
+    lr.on('end',function(){
+        var record = JSON.stringify({
+            title:content[0],
+            source:content[1],
+            url:content[2],
+            time:content[3],
+            body:body
+        });
+        var tname = S(filename).right(8).s;
+        tname = S(tname).left(6).s;
+
+        import_record_nums++;
+        setTimeout(function(){
+            import2db(dbname,tname,record);
+        },import_record_nums*1000);
+
+        if(import_record_nums>600){
+            import_record_nums=1;
+            lr.pause();
+            setTimeout(function(){
+                lr.resume();
+            },60*1000);
+
+        }
+
+        content = [];
+        //console.log("read ["+filename+"] done");
+        fin(filename);
+    });
+}
+function import2db(dname,tname,content){
+    var date = dateFormat(new Date(), "yyyymmdd");
+    var for_id = JSON.parse(content);
+    var url = for_id['url'];
+    client.create({
+        index:dname,
+        type:tname,
+        id:url,
+        body:content
+    },function(error,response){
+        if(!error&&!response.error){
+            /*
+            var result = JSON.stringify(response);
+            fs.appendFile("logs/success.log",result+"\n",function(err){
+                if(err){
+                    //console.log("write log false:"+error);
+                }
+            });
+            */
+        }
+        else{
+            //console.log("write log false");
+            var temp="",code="";
+            if(response!==undefined){
+                temp = JSON.stringify(response.error);
+                code = response.status;
+                if(code!="409"&&code!="200"&&code!="201"){
+                    fs.appendFile("logs/err_"+date+".content",content+"\n",function(err){
+                        if(err){
+                            //console.log("write log false:"+error);
+                        }
+                    });
+
+                }
+            }
+            else{
+                temp = error;
+                code = "503";
+            }
+
+            if(code=="503"){
+                fs.appendFile("logs/restart_"+date+".log","code:"+code+"\n"+temp+"\ncontent:"+content+"--\n",function(err){
+                    if(err){
+                        //console.log("write log false:"+error);
+                    }
+
+                });
+                console.log("Sleep for 1 munutes...");
+                sleep.sleep(60);
+                console.log("Reimport after 1 minutes");
+                setTimeout(function(){
+                    import2db(dname,tname,content);
+                },60*1000);
+            }
+            else if(code!="409"){
+                fs.appendFile("logs/err_"+date+".log","code:"+code+"\n"+temp+"\n--\n",function(err){
+                    if(err){
+                        //console.log("write log false:"+error);
+                    }
+
+                });
+            }
+        }
+    });
+
+}
